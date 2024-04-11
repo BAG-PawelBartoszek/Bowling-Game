28eba8ead1201f274762965a8cd7523291c01c8f
Pawel Bartoszek <pawel.bartoszek@brockhaus-ag.de>
2024-04-11 18:58:27 +0200

    Java Musterimplementierung von Uncle Bob

--- bowling-game-kata/src/main/java/de/brockhausag/codingdojo/bowlinggame/BowlingGameImpl.java
+++ bowling-game-kata/src/main/java/de/brockhausag/codingdojo/bowlinggame/BowlingGameImpl.java
@@ -1,13 +1,52 @@
 package de.brockhausag.codingdojo.bowlinggame;
 
-public class BowlingGameImpl implements BowlingGame {
-    @Override
-    public void roll(int numberOfPins) {
+import java.util.List;
 
+public class BowlingGameImpl implements BowlingGame {
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
     }
 
     @Override
     public int getScore() {
-        return 0;
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
     }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex+1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex+2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex+1] + rolls[frameIndex+2];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1] == 10;
+    }
+
 }
